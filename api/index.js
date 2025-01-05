b5a0577dd6c0a681b3ba034f97598e4d31443052
yash00097 <yashwanthvuppala77@gmail.com>
2025-01-05 22:05:06 +0530

    created database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
 
+
+dotenv.config();
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
+    connectDB();
 });
