a0fd7beb0413b9a03c1fb9d7662aa65873afd209
yash00097 <yashwanthvuppala77@gmail.com>
2025-05-31 22:32:43 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
+import userRoutes from './routes/user.route.js';
 
 
 dotenv.config();
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use('/api/user', userRoutes);
+
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
