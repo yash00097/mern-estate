ac5a7e237a8075c57afac5421f3438b13225c112
yash00097 <yashwanthvuppala77@gmail.com>
2025-06-07 18:06:06 +0530

    small change

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -64,7 +64,7 @@ export default function SignIn() {
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign In'}
-        </button>More actions
+        </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
