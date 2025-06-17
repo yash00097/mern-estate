e05b9332fcb6a3eb1fa097b7b8bc4ceaadb7a54b
yash00097 <yashwanthvuppala77@gmail.com>
2025-06-17 12:22:23 +0530

    create update user api

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 
+ 
 
 dotenv.config();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cookieParser());
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
