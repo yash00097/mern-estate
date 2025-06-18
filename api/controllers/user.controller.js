5281116a04b1f849662c03f60b8cb903292fb8b8
yash00097 <yashwanthvuppala77@gmail.com>
2025-06-18 14:45:58 +0530

    delete User

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,3 +31,21 @@ export const updateUser = async (req, res, next) => {
         next(err);
     }
 }
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id){
+        return next(errorHandler(res, 401, 'you can only delete your own account!'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie("access_token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        });
+        res.status(200).json('User has been deleted successfully!');
+        
+    } catch (error) {
+        next(error);
+    }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
 } from '../redux/user/userSlice';
 
 const Profile = () => {
@@ -66,6 +69,22 @@ const Profile = () => {
       });
     }
   };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.error));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      deleteUserFailure(error.message);
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -112,7 +131,7 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error || ''}</p>
