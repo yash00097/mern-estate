c2309ec18da699b17eef44c5e97cf9165d959dfb
yash00097 <yashwanthvuppala77@gmail.com>
2025-07-03 12:07:41 +0530

    list deletion

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,3 +14,24 @@ export const createListing = async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'Listing not found'));
+    }
+    if(listing.user.toString() !== req.user.id){
+        return next(errorHandler(401, 'You can only delete your own listing'));
+    }
+    try {
+        await listing.deleteOne();
+        res.status(200).json({
+            message: 'Listing deleted successfully',
+        });
+        
+    } catch (error) {
+        next(error);
+    }
+
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -179,6 +179,21 @@ const Profile = () => {
       setShowListingsError(true);
     }
   };
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.error);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
 
   return (
@@ -322,7 +337,12 @@ const Profile = () => {
               </Link>
   
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 dark:text-red-400 uppercase'>Delete</button>
+                <button 
+                  onClick={() => handleDeleteListing(listing._id)} 
+                  className='text-red-700 dark:text-red-400 uppercase'
+                  >
+                    Delete
+                </button>
                 <button className='text-green-700 dark:text-green-400 uppercase'>Edit</button>
               </div>
             </div>
