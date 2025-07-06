9d0117a390d0570bceff310dacd53b95f7b201ed
yash00097 <yashwanthvuppala77@gmail.com>
2025-07-06 23:08:26 +0530

    removed expire token

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,7 +46,6 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign(
             { id: validUser._id , role: validUser.role },
             process.env.JWT_SECRET,
-            { expiresIn: '1d' } 
         );
         const { password: pass , ...userWithoutPassword } = validUser._doc;
         res
@@ -68,7 +67,7 @@ export const googleAuth = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email })
     if (user) {
-      const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+      const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET,);
       const { password: pass, ...rest } = user._doc;
       res
         .cookie('access_token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000, secure: process.env.NODE_ENV === 'production', sameSite: 'strict' })
@@ -80,7 +79,7 @@ export const googleAuth = async (req, res, next) => {
       const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
       const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4) , email: req.body.email, password: hashedPassword, avatar: req.body.photo });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id, role: newUser.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+      const token = jwt.sign({ id: newUser._id, role: newUser.role }, process.env.JWT_SECRET, );
       const { password: pass, ...rest } = newUser._doc;
       res.cookie('access_token', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000, secure: process.env.NODE_ENV === 'production', sameSite: 'strict' }).status(200).json(rest);
       
