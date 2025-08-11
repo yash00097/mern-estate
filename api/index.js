818e895adc5a24ba8db19de04474848a7e5db818
yash00097 <yashwanthvuppala77@gmail.com>
2025-08-11 17:41:51 +0530

    small change in index.js

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/listing',listingRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
